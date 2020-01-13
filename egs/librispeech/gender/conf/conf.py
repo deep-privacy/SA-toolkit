bfb605c6e9d2d380062a5ab0fe442fa3fe474d03
pchampio <prr.champion@gmail.com>
2020-01-13 12:40:10 +0100

    move from x-vector to a lstm

--- egs/librispeech/gender/conf/conf.py
+++ egs/librispeech/gender/conf/conf.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import os
-from damped import nets
 from damped.utils import gender_mapper
 import torch
 import torch.nn as nn
@@ -15,26 +14,53 @@ import torch.nn as nn
 # - `mapper` to map the distirb y label to the domain task y label
 
 
-# Assuming 1024 dim per frame (T)
-frame1 = nets.TDNN(input_dim=1024, output_dim=512, context_size=5, dilation=1)
-frame2 = nets.TDNN(input_dim=512, output_dim=512, context_size=3, dilation=2)
-frame3 = nets.TDNN(input_dim=512, output_dim=512, context_size=3, dilation=3)
-frame4 = nets.TDNN(input_dim=512, output_dim=512, context_size=1, dilation=1)
-frame5 = nets.TDNN(input_dim=512, output_dim=1500, context_size=1, dilation=1)
-# Input to frame1 is of shape (batch_size, T, 24)
-# Output of frame5 will be (batch_size, T-14, 1500)
+# Assuming 1024 dim per frame (T) (encoder projection)
+class GenderNet(nn.Module):
+    """ Gender classification net
+    """
 
-net = nn.Sequential(
-    frame1,
-    frame2,
-    frame3,
-    frame4,
-    frame5,
-    nets.StatsPooling(),  # mean + std (out_dim = 2x in_dim = 3000) over frame5
-    nets.DenseEmbedding(in_dim=3000, mid_dim=512, out_dim=512),
-    nn.Linear(512, 2),
-    nn.Softmax(dim=1),
-)
+    def __init__(self):
+        super(GenderNet, self).__init__()
+        self.eproj = 50
+        self.hidden_size = 512
+        self.num_layers = 1
+
+        self.lstm = nn.LSTM(
+            self.eproj,
+            self.hidden_size,
+            self.num_layers,
+            batch_first=True,
+            dropout=0.2,
+        )
+        self.fc1 = nn.Linear(self.hidden_size, 2)
+        self.softmax = nn.Softmax(dim=1)
+
+    def zero_state(self, hs_pad):
+        return hs_pad.new_zeros(self.num_layers, hs_pad.size(0), self.hidden_size)
+
+    def forward(self, hs_pad):
+        """Gender branch forward
+        Args:
+            hs_pad (torch.Tensor): batch of padded hidden state sequences (B, Tmax, D)
+        """
+        h_0 = self.zero_state(hs_pad)
+        c_0 = self.zero_state(hs_pad)
+
+        self.lstm.flatten_parameters()  # Memory: compact weights
+
+        out_lstm, (h_0, c_0) = self.lstm(hs_pad, (h_0, c_0))
+
+        h_0 = h_0[0]  # only one layer*direction
+
+        out_fc1 = self.fc1(h_0)
+
+        out = self.softmax(out_fc1)
+
+        return out
+
+
+net = GenderNet()
+
 
 #  Binary Cross Entropy
 criterion = nn.CrossEntropyLoss()
