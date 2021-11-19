b0afb49c33e1da44ace7ebf31225237198f9960c
pchampio <prr.champion@gmail.com>
2021-11-19 13:13:12 +0100

    fix sidekit vpc get all score

--- sidekit/egs/vpc2020_data_split/get_all_scores.sh
+++ sidekit/egs/vpc2020_data_split/get_all_scores.sh
@@ -2,29 +2,29 @@
 
 pkwrap_libri_path="$HOME/lab/asr-based-privacy-preserving-separation/pkwrap/egs/librispeech/v1/"
 
-# ./local/compute_metrics.sh \
-    # --pkwrap_model "local/chain/e2e/tuning/tdnnf.py" \
-    # --pkwrap_bn_dim 256 \
-    # --pkwrap_vq_dim -1 \
-    # --pkwrap_exp_dir "exp/chain/e2e_tdnnf/" \
-    # --asv_model ../../egs/libri460_fast2/model/best_model.pt 2> /dev/null
+./local/compute_metrics.sh \
+    --pkwrap_model "local/chain/e2e/tuning/tdnnf.py" \
+    --pkwrap_bn_dim 256 \
+    --pkwrap_vq_dim -1 \
+    --pkwrap_exp_dir "exp/chain/e2e_tdnnf/" \
+    --asv_model ../../egs/libri460_fast2/model/best_model.pt 2> /dev/null
 
-# exp=e2e_tdnnf
-# for suff in dev_clean_fbank_hires test_clean_fbank_hires test_other_fbank_hires; do
-    # if [ ! -f "${pkwrap_libri_path}/exp/chain/$exp/decode_${suff}_iterfinal_final_fg/scoringDetails/wer_details/wer_bootci" ]; then
-        # cd $pkwrap_libri_path
-        # . ./path.sh
-        # ./local/wer_detail.sh \
-        # --dataDir ./data/$suff \
-        # --decodeDir ${pkwrap_libri_path}/exp/chain/$exp/decode_${suff}_iterfinal_final_fg \
-        # --langDir data/lang_lp_test_fglarge
-        # cd -
-    # fi
-    # echo "WER bootci $(echo $suff | cut -d_ -f1,2 ): $(cat ${pkwrap_libri_path}/exp/chain/$exp/decode_${suff}_iterfinal_final_fg/scoringDetails/wer_details/wer_bootci)"
-# done
+exp=e2e_tdnnf
+for suff in dev_clean_fbank_hires test_clean_fbank_hires test_other_fbank_hires; do
+    if [ ! -f "${pkwrap_libri_path}/exp/chain/$exp/decode_${suff}_iterfinal_final_fg/scoringDetails/wer_details/wer_bootci" ]; then
+        cd $pkwrap_libri_path
+        . ./path.sh
+        ./local/wer_detail.sh \
+        --dataDir ./data/$suff \
+        --decodeDir ${pkwrap_libri_path}/exp/chain/$exp/decode_${suff}_iterfinal_final_fg \
+        --langDir data/lang_lp_test_fglarge
+        cd -
+    fi
+    echo "WER bootci $(echo $suff | cut -d_ -f1,2 ): $(cat ${pkwrap_libri_path}/exp/chain/$exp/decode_${suff}_iterfinal_final_fg/scoringDetails/wer_details/wer_bootci)"
+done
 
 
-for dim in 256 16 32 48 64 128 384 512 768 ;do
+for dim in 16 32 48 64 128 256 384 512 768 ;do
 
     ./local/compute_metrics.sh \
         --pkwrap_model "local/chain/e2e/tuning/tdnnf_vq.py" \
@@ -32,8 +32,6 @@ for dim in 256 16 32 48 64 128 384 512 768 ;do
         --pkwrap_vq_dim $dim \
         --pkwrap_exp_dir "exp/chain/e2e_tdnnf_vq_sizeco_$dim/" \
         --asv_model ../../egs/libri460_fast_vq/model/best_model_vq_$dim.pt 2> /dev/null
-
-    exit 0
 
     exp=e2e_tdnnf_vq_sizeco_$dim
     for suff in dev_clean_fbank_hires test_clean_fbank_hires test_other_fbank_hires; do
