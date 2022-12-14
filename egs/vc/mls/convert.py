141eecb29fc0c2ddbc44fbde662691f397749178
hnourtel <hubert.nourtel@loria.fr>
2022-12-14 17:39:53 +0100

    Refactoring convert script

--- egs/vc/mls/convert.py
+++ egs/vc/mls/convert.py
@@ -15,52 +15,7 @@ from tqdm import tqdm
 import satools
 
 
-def stream(message):
-    sys.stdout.write(f"\r{message}")
-
-
-def progbar(i, n, size=16):
-    done = (i * size) // n
-    bar = ""
-    for i in range(size):
-        bar += "█" if i <= done else "░"
-    return bar
-
-
-def convert(sample, target=None):
-    waveform, lengths, filename, f0, ys = sample
-
-    if target == None:
-        audio = forward_synt(
-            audio=waveform.to(demo.device).clone(),
-            f0=f0.to(demo.device),
-            real_shape=lengths,
-        )
-    else:
-        global wav2utt
-        _target = []
-        for f in filename:
-            _target.append(target[wav2utt[f]])
-        audio = forward_synt(
-            audio=waveform.to(demo.device).clone(),
-            f0=f0.to(demo.device),
-            real_shape=lengths,
-            target=_target,
-        )
-
-    def parallel_write():
-        for i, f in enumerate(filename):
-            fname_out_name = Path(f).stem
-            output_file = os.path.join(out_dir, fname_out_name + "_gen.wav")
-            wavfile.write(output_file, synthesis_sr, audio[i])
-
-    p = Process(target=parallel_write, args=())
-    p.start()
-    return p
-
-
-if __name__ == "__main__":
-
+def main():
     print("Initializing Inference Process..")
 
     """
@@ -79,6 +34,7 @@ if __name__ == "__main__":
     parser.add_argument("--batch-size", type=int, default=4)
     parser.add_argument("--num-workers", type=int, default=2)
     parser.add_argument("--extract-f0-only", action="store_true")
+    parser.add_argument("--f0-cache-file", type=str, default=".f0_sr-320.cache")
     parser.add_argument("--in", type=str, dest="_in")
     parser.add_argument("--in-wavscp", type=str, dest="_in_scp", default=None)
     parser.add_argument("--target_id", type=str, default=None)
@@ -87,9 +43,9 @@ if __name__ == "__main__":
     parser.add_argument("--vq-dim", type=int, dest="vq_dim", default=-1)
     parser.add_argument("--dp-e", type=int, dest="dp_dim", default=0)
     parser.add_argument("--model-type", type=str, default="tdnnf")
-    parser.add_argument(
-        "--rand-pitch", type=str, default="False", help="Randomize the pitch shape"
-    )
+    parser.add_argument("--spk-in-out-name", type=str, default="False", help="Indicate if we add speaker and book in output filenames. Useful if original filename doesn't contains speaker or book id")
+    parser.add_argument("--pitch-target-noise-db", type=int, default=0, help="Add noise to pitch (randomization of the pitch). Value in DB")
+    parser.add_argument("--pitch-quant", type=str, default="True", help="Apply quantification on pitch")
     parser.add_argument(
         "--dp-pitch",
         type=str,
@@ -104,29 +60,22 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    global forward_synt
-    global synthesis_sr
     global wav2utt
-    synthesis_sr = 16000
-    global out_dir
 
     if os.path.exists(args.f0_stats):
         # File provided for f0_stats, open it before going through json loading
         with open(args.f0_stats, 'r') as f0_stats_file:
             f0_stats = json.loads(f0_stats_file.read())
     else:
+        # Directly json format string provided for f0_stats, parse it directly
         f0_stats = json.loads(args.f0_stats.replace("'", '"'))
 
-
-    #  dim = 128
-    #  root_data = "/lium/home/pchampi/lab/asr-based-privacy-preserving-separation/satools/egs/librispeech/v1/corpora/LibriSpeech/train-clean-360"
-    #  out_dir = "generated_train-clean-360_vq_" + str(dim)
-
+    spk_in_out_name = True if args.spk_in_out_name.lower() == "true" else False
+    pitch_quant = True if args.pitch_quant.lower() == "true" else False
     audio_extension = args.ext
     dim = args.vq_dim
     dp_dim = args.dp_dim
     out_dir = args._out
-
     os.makedirs(out_dir, exist_ok=True)
 
     if args.target_id != None:
@@ -138,7 +87,7 @@ if __name__ == "__main__":
         wavs_path = list(wavs_scp.values())
         wavs_path = list(demo.split(wavs_path, args.of))[args.part]
         torch_dataset = satools.hifigan.dataset.WavList(
-            wavs_path, load_func=satools.utils.kaldi.load_wav_from_scp
+            wavs_path, load_func=satools.utils.kaldi.load_wav_from_scp, filter_dir=out_dir
         )
     else:
         root_data = args._in
@@ -156,8 +105,8 @@ if __name__ == "__main__":
                     pbar.set_description(f"audio file count : {wav_count}")
                     wavs_path.append(file_path)
 
-            #  if len(wavs_path) > 10:
-            #  break
+            # if len(wavs_path) > 10:
+            #     break
 
         # TODO implement wav2utt required by any to many models
         wavs_path = list(demo.split(wavs_path, args.of))[args.part]
@@ -178,27 +127,27 @@ if __name__ == "__main__":
         persistent_workers=True,
     )
 
-    if args.rand_pitch.lower() == "true" or args.dp_pitch != "0":
-        if args.rand_pitch.lower() == "true":
-            target_noise_db = 5  # HARD CODED
-            #  target_noise_db = 15  # HARD CODED
-            print("---> TARGET_NOISE_DB:", target_noise_db)
-
-        print("Apply modification on the F0 shape")
-
     if args.extract_f0_only:
         print("Only extracting F0 features")
+        satools.hifigan.f0.set_cache_file(args.f0_cache_file)
         satools.hifigan.f0.set_yaapt_opts({
             "frame_length": 35.0,
             "frame_space": 20.0,
             "nccf_thresh1": 0.25,
             "tda_frame_length": 25.0,
         })
-        for i, sample in enumerate(dataloader):
-            bar = progbar(i * batch_size, len(wavs_path))
-            message = f"{bar} {i*batch_size}/{len(wavs_path)} "
-            stream(message)
+        for sample in tqdm(dataloader):
+            pass
         sys.exit(0)
+
+    target_noise_db = 0
+    if args.pitch_target_noise_db > 0:
+        print("Apply random noise on the F0 shape")
+        target_noise_db = args.pitch_target_noise_db
+        print("---> TARGET_NOISE_DB:", target_noise_db)
+
+    if args.dp_pitch != "0":
+        print("Apply differential privacy on the F0 shape")
 
     # Loading model configuration from json config file
     with open("convert_config.json", 'r') as config_file:
@@ -222,11 +171,19 @@ if __name__ == "__main__":
     else:
         raise ValueError(f"Model type {args.model_type} not found in config file")
 
+    if torch.cuda.is_available():
+        device = torch.device("cuda:0")
+    else:
+        device = torch.device("cpu")
+
+    # TODO : rewrite init functions to have only one entry point for each case (wav2vec2, differential privacy, vq, etc.)
     forward_asr, pk_model = demo.init_asr_model(
         model=asr_model_config["model"],
         exp_path=eval(compile(asr_model_config["exp_path"], 'exp_path', 'eval')) if asr_model_config["exp_path"][0] == "f" else asr_model_config["exp_path"],
         load_model=asr_model_config["load_model"],
-        egs_path=asr_model_config["egs_path"]
+        egs_path=asr_model_config["egs_path"],
+        device=device,
+        vq_dim=dim
     )
     forward_synt, synt_model = demo.init_synt_hifigan_w2v2(
         model=synt_model_config["model"],
@@ -235,10 +192,14 @@ if __name__ == "__main__":
         model_weight=synt_model_config["model_weight"],
         egs_path=synt_model_config["egs_path"],
         json_stats_file=synt_model_config["json_stats_file"],
+        device=device
     )
 
     if (
-        (args.model_type == "wav2vec2" or args.model_type == "libritts_tdnnf" or args.model_type == "wav2vec2_mls")
+        (args.model_type == "wav2vec2"
+            or args.model_type == "libritts_tdnnf"
+            or args.model_type == "wav2vec2_mls"
+            or args.model_type == "wav2vec2_no_vq")
         and os.getenv("TARGET_single", default="false") != "true"
         and args.f0_stats != parser.get_default("f0_stats")
     ):
@@ -273,14 +234,20 @@ if __name__ == "__main__":
             print(f"Loading DP-Pitch AE model from {gae_path}")
             gae_model = load_AE(conf_filepath, dp_device, gae_path, DP_eps)
 
+        def quantize_f0_torch(x, num_bins=16):
+            B = x.size(0)
+            A = x.size(1)
+            x = x.view(-1).clone()
+            uv = (x == 0)
+            x = torch.round(x * (num_bins)) / (num_bins)
+            x[uv] = 0
+            return x.view(B, A, -1)
+
         def _norm(f0, f0_stats, filename):
             spk_id = spk2target[filename2wav[filename]]
             pitch = f0
 
-            if args.dp_pitch == "0":
-                pitch = satools.hifigan.f0.m_std_norm(pitch, f0_stats[spk_id], filename)
-
-            if args.rand_pitch.lower() == "true":
+            if target_noise_db > 0:
                 # Set a target channel noise power to something very noisy
                 # Convert to linear Watt units
                 target_noise_watts = 10 ** (target_noise_db / 10)
@@ -293,8 +260,11 @@ if __name__ == "__main__":
                 ii = pitch == 0
                 pitch = pitch + torch.tensor(noise_volts, dtype=pitch.dtype)
                 pitch[ii] = 0
-
-            if args.dp_pitch != "0":
+            elif pitch_quant:
+                pitch = quantize_f0_torch(pitch, num_bins=2)
+            elif args.dp_pitch == "0":
+                pitch = satools.hifigan.f0.m_std_norm(pitch, f0_stats[spk_id], filename)
+            elif args.dp_pitch != "0":
                 pitch = pitch.squeeze()
 
                 # Have a record of indexes for puting back zeros later
@@ -325,20 +295,68 @@ if __name__ == "__main__":
 
         satools.hifigan.f0.set_norm_func(_norm)
     else:
-        print(
-            "Targetting single speaker F0!", args.f0_stats, "(default LJspeech speaker)"
-        )
+        print("Targetting single speaker F0!", args.f0_stats)
 
-    for i, sample in enumerate(dataloader):
-        if args.target_id != None:
-            p = convert(sample, target=spk2target)
-        else:
-            p = convert(sample)
-        bar = progbar(i * batch_size, len(wavs_path))
-        message = f"{bar} {i*batch_size}/{len(wavs_path)} "
-        stream(message)
-    bar = progbar(len(wavs_path), len(wavs_path))
-    message = f"{bar} {len(wavs_path)}/{len(wavs_path)} "
-    stream(message)
+    for i, sample in enumerate(tqdm(dataloader)):
+        try:
+            p = convert(sample, device=device, forward_synt=forward_synt, out_dir=out_dir, target=spk2target if args.target_id != None else None, spk_book_in_name=spk_in_out_name)
+        except RuntimeError as e:
+            print("GPU memory error. Running current batch with one file at the same time")
+            for samp_itm in range(sample[1].size(0)):
+                # Selecting data for one sample in the batch and formatting for model input
+                cur_samp = (sample[0][samp_itm].unsqueeze(0),
+                            sample[1][samp_itm].unsqueeze(0),
+                            [sample[2][samp_itm]],
+                            sample[3][samp_itm].unsqueeze(0),
+                            sample[4][samp_itm].unsqueeze(0))
+                # Run conversion on current sample with size 1
+                try:
+                    p = convert(cur_samp, device=device, forward_synt=forward_synt, out_dir=out_dir, target=spk2target if args.target_id != None else None, spk_book_in_name=spk_in_out_name)
+                except RuntimeError as e:
+                    print(f"Cannot process current file : {cur_samp[2]}\nIgnoring current file and continue to the next one.")
+        finally:
+            torch.cuda.empty_cache()
+
     # wait for last p to write
     p.join()
+
+
+def convert(sample, device, forward_synt, out_dir, target=None, synthesis_sr=16000, spk_book_in_name=False):
+    waveform, lengths, filename, f0, ys = sample
+
+    if target == None:
+        audio = forward_synt(
+            audio=waveform.to(device).clone(),
+            f0=f0.to(device),
+            real_shape=lengths,
+        )
+    else:
+        global wav2utt
+        _target = []
+        for f in filename:
+            _target.append(target[wav2utt[f]])
+        audio = forward_synt(
+            audio=waveform.to(device).clone(),
+            f0=f0.to(device),
+            real_shape=lengths,
+            target=_target,
+        )
+
+    def parallel_write(synthesis_sr, spk_book_in_name):
+        for i, f in enumerate(filename):
+            if spk_book_in_name:
+                split_cur_file = f.split("/")
+                fname_out_name = split_cur_file[-3] + "_" + split_cur_file[-2] + "_" + split_cur_file[-1].split(".")[0]
+            else:
+                fname_out_name = Path(f).stem
+            output_file = os.path.join(out_dir, fname_out_name + "_gen.wav")
+            wavfile.write(output_file, synthesis_sr, audio[i])
+
+    parallel_write(synthesis_sr, spk_book_in_name)
+    p = Process(target=parallel_write, args=())
+    p.start()
+    return p
+
+
+if __name__ == "__main__":
+    main()
