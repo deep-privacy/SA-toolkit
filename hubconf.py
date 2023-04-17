f26c87a333bcf72edbc9776f8380534c3603c312
pchampio <prr.champion@gmail.com>
2023-04-18 00:38:55 +0200

    hub

--- hubconf.py
+++ hubconf.py
@@ -3,15 +3,15 @@ dependencies = ['torch', 'torchaudio', 'numpy']
 
 from satools.infer_helper import load_model
 
-def anonymization(pretrained=True, model='hifigan_bn_tdnnf_wav2vec2_vq_48_v1', device='cpu'):
+def anonymization(pretrained=True, tag_version='hifigan_bn_tdnnf_wav2vec2_vq_48_v1', device='cpu'):
     """Loads an anonymization model
 
     Arguments:
         pretrained (bool): load pretrained weights into the model
-        model (str): the tag/release from github corresponding to a model type (check: https://github.com/deep-privacy/SA-toolkit/releases)
+        tag_version (str): the tag/release from github corresponding to a model type (check: https://github.com/deep-privacy/SA-toolkit/releases)
         device (str): 'cuda' or 'cpu'
     Returns:
         torch.nn.model
     """
-    weight_url = f"https://github.com/deep-privacy/SA-toolkit/releases/download/{model}/final.pt"
+    weight_url = f"https://github.com/deep-privacy/SA-toolkit/releases/download/{tag_version}/final.pt"
     return load_model(weight_url)
