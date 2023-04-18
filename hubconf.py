bb488687968b986bae937efe3bd1f83f7bb33b5c
pchampio <prr.champion@gmail.com>
2023-04-18 02:11:36 +0200

    fix

--- hubconf.py
+++ hubconf.py
@@ -21,12 +21,11 @@ def anonymization(pretrained=True, tag_version='hifigan_bn_tdnnf_wav2vec2_vq_48_
     matching_dirs = [d for d in dir_list if d.startswith('deep-privacy_SA-toolkit')]
 
     if len(matching_dirs) > 0:
-        sys.path.insert(0,f"{os.path.join(local_hub_dir, matching_dirs[0])}/deep-privacy_SA-toolkit_refactoring/satools/")
+        sys.path.insert(0,f"{os.path.join(local_hub_dir, matching_dirs[0])}/satools/")
     else:
         print(f"No matching {local_hub_dir} + 'deep-privacy_SA-toolkit*' directory found.")
         sys.exit(1)
-    import satools
+    from satools.infer_helper import load_model
 
     weight_url = f"https://github.com/deep-privacy/SA-toolkit/releases/download/{tag_version}/final.pt"
-    return satools.infer_helper.load_model(weight_url)
-
+    return load_model(weight_url)
